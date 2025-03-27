7542f5baa5f0ba00fa13d8c72b2b232ad57a960d
MaxTrinnity <awareonmusic@gmail.com>
2025-03-27 11:08:06 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,9 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request));
 });
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js');
+  }
+</script>
+
