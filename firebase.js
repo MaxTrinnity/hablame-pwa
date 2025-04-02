aa511188440a0b8bbda986545f670ae032e2a8a0
MaxTrinnity <awareonmusic@gmail.com>
2025-04-02 14:05:22 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPEE4EetYFCR0rqL5LlQN_46u2yh3iqOQ",
+  authDomain: "hablame-islandes.firebaseapp.com",
+  projectId: "hablame-islandes",
+  storageBucket: "hablame-islandes.appspot.com",
+  messagingSenderId: "150210962920",
+  appId: "1:150210962920:web:1ed6c3ceeb16bf2c5cf705d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
