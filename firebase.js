ba8246c9a1c2b688fcb0c2c706dd4200b440481b
MaxTrinnity <awareonmusic@gmail.com>
2025-04-06 20:49:05 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCPEE4EetYFCR0rqL5LlQN_46u2yh3iqOQ",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
